6c862892b697c001576bfb3fea40fbffab0e92cb
powershellshock <jaredlp@outlook.com>
2023-03-29 17:25:48 -0400

    refactoring from reading ref implementations

--- private/bech32.ps1
+++ private/bech32.ps1
@@ -21,6 +21,18 @@
 ## Credit: https://github.com/sipa/bech32/blob/master/ref/javascript/bech32.js
 
 $CHARSET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l'
+
+[sbyte[]]$CHARSET_REV = @(
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  15, -1, 10, 17, 21, 20, 26, 30,  7,  5, -1, -1, -1, -1, -1, -1,
+  -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+   1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1,
+  -1, 29, -1, 24, 13, 25,  9,  8, 23, -1, 18, 22, 31, 27, 19, -1,
+   1,  0,  3, 16, 11, 28, 12, 14,  6,  4,  2, -1, -1, -1, -1, -1
+)
+
 $GENERATOR = @(0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3)
 
 $encodings = @{
@@ -28,17 +40,13 @@ $encodings = @{
     BECH32M = "bech32m"
 }
 
+enum Bech32_Encoding {
+  bech32 = 1
+  bech32m = 0x2bc830a3
+}
 
-
-
-
-
-
-function getEncodingConst {
-  #[OutputType([int])]
-  Param(
-    [string]$enc
-  )
+function [int]getEncodingConst ([string]$enc) {
+    <#
     if ($enc -eq $encodings.BECH32) {
         return 1
     } elseif ($enc -eq $encodings.BECH32M) {
@@ -46,18 +54,15 @@ function getEncodingConst {
     } else {
         return $null
     }
+    #>
+    return ($enc -as [int])
 }
 
-function polymod {
-  #[OutputType([int])]
-  Param(
-    [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
-    [string]$values
-  )
-    $chk = 1
+function  [int]polymod ([string]$values) {
+    [int]$chk = 1
     for ($p = 0; $p -lt $values.Length; ++$p) {
-        $top = $chk -shr 25
-        $chk = ($chk -band 0x1ffffff) -shl 5 -bxor $values[$p]
+        [int]$top = $chk -shr 25
+        $chk = (($chk -band 0x1ffffff) -shl 5) -bxor $values[$p]
         for ($i = 0; $i -lt 5; ++$i) {
             if (($top -shr $i) -band 1) {
                 $chk = $chk -bxor $GENERATOR[$i]
@@ -67,62 +72,57 @@ function polymod {
     return $chk
 }
 
-function hrpExpand {
-  [OutputType([string])]
-  Param(
-    [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
-    [ValidateNotNullOrEmpty()]
-    [string]$hrp
-  )  
-    [string]$ret = @()
+function [byte[]]hrpExpand ([string]$hrp) {  
+    [byte[]]$ret = @()
     #$bigInt = [Numerics.BigInteger]::Zero
     for ($p = 0; $p -lt $hrp.Length; ++$p) {
-      $ret += ([byte][char]$hrp[$p]) -shr 5
+      #$ret += [byte][char]($hrp[$p] -shr 5)
+      $ret[$p] = [byte]($hrp[$p] -shr 5)
     }
     $ret += 0
     for ($p = 0; $p -lt $hrp.Length; ++$p) {
-      $ret += ([byte][char]$hrp[$p]) -band 31
+      #$ret += [byte][char]($hrp[$p] -band 31)
+      $ret[$p] = [byte]($hrp[$p] -band 31)
     }
     return $ret
 }
 
-function verifyChecksum ([string]$hrp, $data, [string]$enc) {
-  return ((polymod(((hrpExpand($hrp)) + $data))) -eq (getEncodingConst($enc)))
+function [Bech32_Encoding]verifyChecksum ([string]$hrp, [byte[]]$data) {
+  [int]$chk = polymod([string](hrpExpand($hrp),$data))
+  return $chk
+  #return ((polymod(((hrpExpand($hrp)) + $data))) -eq (getEncodingConst($enc)))
 }
 
-function createChecksum ([string]$hrp, $data, $enc) {
+function [byte[]]createChecksum ([string]$hrp, [byte[]]$data, [Bech32_Encoding]$enc) {
   $values = (hrpExpand($hrp)) + $data + [string]@(0,0,0,0,0,0)
-  $mod = polymod($values) -bxor (getEncodingConst($enc))
+  #$mod = polymod($values) -bxor (getEncodingConst($enc))
+  [int]$mod = (polymod($values) -bxor ($enc -as [int]))
   $ret = @()
   for ($i=0; $i -lt 6; ++$i) {
-      $ret += (($mod -shr (5 * (5 - $i))) -band 31)
+      #$ret += (($mod -shr (5 * (5 - $i))) -band 31)
+      $ret[$i] = (($mod -shr (5 * (5 - $i))) -band 31)
     }
     return $ret
 }
 
-function ConvertTo-Bech32 ($hrp, $data, $enc) {
+function [string]ConvertTo-Bech32 ([string]$hrp, [byte[]]$data, [Bech32_Encoding]$enc) {
   #Write-Output "$([string]$hrp)'1$(([string]([int[]]$data + [int[]](createChecksum($hrp,$data,$enc))))|%{[char]$CHARSET[$_]}|Out-String)" 
-  $combined = $data + @(createChecksum($hrp, $data, $enc))
-  $ret = $hrp + '1'
+  [byte[]]$combined = $data + @(createChecksum($hrp, $data, ($enc -as [int])))
+  [string]$ret = $hrp + '1'
+
   for ($p = 0; $p -lt $combined.length; ++$p) {
-    $ret += $CHARSET[($combined[$p])]
+    #$ret += $CHARSET[($combined[$p])]
+    $ret += $CHARSET[$p]
   }
   return $ret
 }
 
-function ConvertFrom-Bech32 {
-  [OutputType([string])]
-  Param(
-    [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
-    $bechString,
-
-    [Parameter(Mandatory=$true,Position=1,ValueFromPipeline=$true)]
-    $enc
-  )
-  $has_lower = $false
-  $has_upper = $false
+function [string]ConvertFrom-Bech32 ([string]$bechString, [Bech32_Encoding]$enc) {
+  [bool]$has_lower = $false
+  [bool]$has_upper = $false
   for ($p = 0; $p -lt $bechString.length; ++$p) {
     $charCode = [byte][char]($bechString[$p])
+    
     if ($charCode -lt 33 -or $charCode -gt 126) {
       return $null
     }
@@ -136,7 +136,9 @@ function ConvertFrom-Bech32 {
   if ($has_lower -and $has_upper) {
     return $null
   }
+  
   $bechString = $bechString.ToLower()
+  
   $pos = $bechString.lastIndexOf('1')
   if ($pos -lt 1 -or $pos + 7 -gt $bechString.length -or $bechString.length -gt 90) {
     return $null
@@ -144,11 +146,14 @@ function ConvertFrom-Bech32 {
   [string]$hrp = $bechString.substring(0, $pos)
   [int[]]$data = @()
   for ($p = $pos + 1; $p -lt $bechString.length; ++$p) {
-    $d = $CHARSET.indexOf($bechString[$p])
+    #$d = $CHARSET.indexOf($bechString[$p])
+    [byte]$c = $bechString[$p + $pos +1]
+    $d = $CHARSET_REV[$c]
     if ($d -eq -1) {
       return $null
     }
-    $data += $d
+    #$data += $d
+    $data[$p] = $d
   }
   <#
   if (!(verifyChecksum($hrp, $data, $enc))) {
@@ -158,6 +163,6 @@ function ConvertFrom-Bech32 {
   $ofs=''
   return @{
     hrp=$hrp
-    data=([string]$CHARSET[$data[0..($data.length - 6)]])
+    data=($data[0..($data.length - 6)])
   }
 }
